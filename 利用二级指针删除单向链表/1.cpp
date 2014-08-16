caf08eb5fde560eac99e87db9dba6568901839c4
daishengdong <daishengdong@gmail.com>
2014-08-16 15:42:07 +0800

    Linus: core low-level coding

---
+++ 利用二级指针删除单向链表/1.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using    namespace    std;
+
+struct node {
+   node *next;
+   int data;
+
+   node() {}
+
+   node(int p_data) {
+       data = p_data;
+       next = NULL;
+   }
+
+   ~node() {
+       cout << data << "\t deleted" << endl;
+   }
+};
+
+node *head = new node(11);
+
+void build_link() {
+    node *cur = head;
+    for (int i = 0; i < 6; ++i) {
+        node *x = new node(i);
+        cur->next = x;
+        cur = x;
+    }
+}
+
+typedef bool (*remove_fn)(node* v);
+
+bool func(node* v) {
+	return v->data & 1;
+}
+
+node *remove_if(node **head, remove_fn rm) {
+	for (node **cur = head; *cur;) {
+        node *entry = *cur;
+        if (rm(entry)) {
+            *cur = entry->next;
+            return entry;
+        } else cur = &entry->next;
+    }
+	return NULL;
+}
+
+
+int main(int argc, char *const argv[]) {
+	build_link();
+
+    node *tmp;
+    while (tmp = remove_if(&head, func)) {
+        cout << tmp->data << endl;
+		delete tmp;
+    }
+
+    return 0;
+}

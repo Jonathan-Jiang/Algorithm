c2ba477c87219368de5a0569d64983972a0a4cf3
daishengdong <daishengdong@gmail.com>
2014-11-03 16:00:53 +0800

    Eratosthenes 素数筛法算法

---
+++ Eratosthenes 素数筛法/1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <math.h>
+using    namespace    std;
+
+void Eratosthenes(bool prime_table[], int n) {
+    int *num = new int[n];
+    for (int i = 0; i < n; ++i) {
+        num[i] = i;
+    }
+
+    prime_table[2] = true;
+    for (i = 2; i <= sqrt(n); ++i) {
+        if (prime_table[i]) {
+            for (int j = 2; j <= n / i; ++j) {
+                prime_table[i * j] = false;
+            }
+        }
+    }
+}
+
+void print(bool prime_table[], int n) {
+    for (int i = 2; i < n; ++i)
+        if (prime_table[i]) cout << i << endl;
+}
+
+int main(int argc, char* const argv[]) {
+    bool prime_table[25];
+    Eratosthenes(prime_table, 25);
+    print(prime_table, 25);
+
+    return 0;
+}
